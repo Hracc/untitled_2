a69aa47626d70dc746e5d7f57a96de13260140eb
Hragun <egor.bazhenoff2016@yandex.ru>
2025-03-30 19:39:19 +0400

    Обновление фронтенда до актуально версии

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,11 @@ import { CategoryPage } from "./pages/CategoryPage";
 import { ServiceDetailsPage } from "./pages/ServiceDetailsPage";
 import { CartPage } from "./pages/CartPage";
 import {Profile} from "./pages/Profile.jsx";
+
+import { TestPage } from "./pages/Test.jsx"
+import { RequestsPage } from "./pages/RequestsPage";
+import { TestOrganization } from "./pages_tests/TestOrganization.jsx"
+
 import "./styles.scss";
 
 
@@ -17,6 +22,10 @@ export default function App() {
                 <Route path="/cart" element={<CartPage />} />
                 {/* Дополнительный маршрут для профиля */}
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/test" element={<TestPage />} />
+                <Route path="/requests" element={<RequestsPage />} />
+                {/* Маршруты для тестирования функционала без вёрстки */}
+                <Route path="/testO" element={<TestOrganization />} />
             </Routes>
         </Router>
     );
---
+++ frontend/src/pages/Test.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { OrganizationForm } from "../components/OrganizationForm";
+import {Header} from "../components/Header.jsx";
+import "../styles.scss";
+
+export function TestPage() {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <div className="content">
+                    <h1>Регистрация организации</h1>
+                    <OrganizationForm />
+                </div>
+            </div>
+        </div>
+    );
+}
